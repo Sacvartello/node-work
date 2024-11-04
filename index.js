dd242b8d905c320231f0d92b34f03f9e0ea966ff
Dgordgun <georgdvorskii@gmail.com>
2024-11-04 14:46:01 +0200

    express

--- index.js
+++ index.js
@@ -1,29 +1,34 @@
 const http = require('http')
+const exp = require('express')
+const app = exp()
 
+const server = http.createServer(app)
 
-const server = http.createServer(reqwList)
-
-function reqwList (req,res){
-    if(req.method === 'GET'){
-        if(req.url === '/about'){
-            res.statusCode = 200;
-            res.statusMessage = 'OK';
-            res.end('this is info about my page');
-        }
-    } 
-    if(req.method === 'POST'){
-        if(req.url === '/users'){
-            let str = ''
-            req.on('data',(chunk)=>{
-                str += chunk
-            })
-            req.on('end', ()=>{
-                const usObj = JSON.parse(str)
-                res.statusCode = 201;
-                res.statusMessage = 'Created';
-                res.end(`${usObj.username} ${usObj.lastName}, ви успішно зареєстровані!`);
-            })
-        }
-    }
-}
+// function reqwList (req,res){
+//     if(req.method === 'GET'){
+//         if(req.url === '/about'){
+//             res.statusCode = 200;
+//             res.statusMessage = 'OK';
+//             res.end('this is info about my page');
+//         }
+//     } 
+//     if(req.method === 'POST'){
+//         if(req.url === '/users'){
+//             let str = ''
+//             req.on('data',(chunk)=>{
+//                 str += chunk
+//             })
+//             req.on('end', ()=>{
+//                 const usObj = JSON.parse(str)
+//                 res.statusCode = 201;
+//                 res.statusMessage = 'Created';
+//                 res.end(`${usObj.username} ${usObj.lastName}, ви успішно зареєстровані!`);
+//             })
+//         }
+//     }
+// }
 server.listen(5000)
+
+app.get('/about',(req, res)=>{
+    res.status(200).send('this is page about ...')
+})
